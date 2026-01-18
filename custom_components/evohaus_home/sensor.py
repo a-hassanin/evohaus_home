adfa2f7508c630b0f7669c1848c7ce8092c33493
Ahmed Hassanin <ahmed.hassanin@sap.com>
2026-01-18 16:32:59 +0100

    Add Parking Space

--- custom_components/evohaus_home/sensor.py
+++ custom_components/evohaus_home/sensor.py
@@ -12,6 +12,7 @@ from homeassistant.const import (
     UnitOfVolume
 )
 
+import re
 from .const import DOMAIN
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
@@ -23,6 +24,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         ElectricityPriceSensor(coordinator),
         ElectricityPriceEuroSensor(coordinator),
         ElectricityMeterSensor(coordinator),
+        ParkingMeterSensor(coordinator),
         ColdWaterBathMeterSensor(coordinator),
         ColdWaterKitchenMeterSensor(coordinator),
         ColdWaterWashMeterSensor(coordinator),
@@ -84,6 +86,82 @@ class EvoSensor(CoordinatorEntity, SensorEntity):
     def _handle_coordinator_update(self):
         self.async_write_ha_state()
 
+
+class ParkingMeterSensor(EvoSensor):
+    def __init__(self, coordinator):
+        meter_data_extracted = self._extract_parking_meter_data(coordinator.data["meter"])
+        super().__init__(coordinator, "Electricity consumption parking " + meter_data_extracted.get("parking_number"), "mdi:meter-electric-outline", "Verbrauch Strom", UnitOfEnergy.KILO_WATT_HOUR, SensorDeviceClass.ENERGY, SensorStateClass.TOTAL_INCREASING)
+
+    @callback
+    def _handle_coordinator_update(self):
+        meter_data_extracted = self._extract_parking_meter_data(self.coordinator.data["meter"])
+        state = meter_data_extracted.get("state")
+
+        if state is not None and int(state) > 0 and (self._attr_native_value is None or state > self._attr_native_value):
+            self._attr_native_value = state
+            self._attr_extra_state_attributes["meter_no"] = meter_data_extracted.get("meter_no")
+            self._attr_extra_state_attributes["parking_number"] = meter_data_extracted.get("parking_number")
+            self._attr_extra_state_attributes["parking_code"] = meter_data_extracted.get("parking_code")
+            super()._handle_coordinator_update()
+
+    def _extract_stellplatz_and_tn(self, input_string):
+        result = {
+            "stellplatz": "",
+            "tn": ""
+        }
+    
+        # Stellplatz: Stpl038, Stlp.242, STPL 12
+        stpl_match = re.search(
+            r'\bStpl\.?\s*(\d+)\b',
+            input_string,
+            re.IGNORECASE
+        )
+    
+        # T-Number: Tn 136, TNr 271
+        tn_match = re.search(
+            r'\bTnR?\s*(\d+)\b',
+            input_string,
+            re.IGNORECASE
+        )
+    
+        if stpl_match:
+            result["stellplatz"] = stpl_match.group(1)
+    
+        if tn_match:
+            result["tn"] = tn_match.group(1)
+    
+        return result
+    
+    def _extract_parking_meter_data(self, meter_data):
+        rows = meter_data.find_all("tr")
+        row = {"state": 0, "meter_no": "", "parking_number": "", "parking_code": ""}
+
+        for raw_row in rows:
+            cols = raw_row.find_all("td")
+            if not cols:
+                continue
+
+            unit = cols[0].contents[0]
+            description = cols[1].contents[0].replace(" " + unit, "")
+            
+            if "Verbrauch Strom" in description:
+                parking_data = self._extract_stellplatz_and_tn(unit)
+                
+                if not bool(parking_data.get("stellplatz") and parking_data.get("tn")):
+                    continue
+                
+                row["state"] = float(
+                    cols[4].contents[0].replace(".", "").replace(",", ".")
+                )
+                row["meter_no"] = cols[2].contents[0]
+                row["parking_number"] = parking_data.get("tn")
+                row["parking_code"] = parking_data.get("stellplatz")
+                return row
+            else:
+                continue
+
+        return row
+
 class MeterSensor(EvoSensor):
     def __init__(self, coordinator, name, icon, tech_name, unit, device_class):
         super().__init__(coordinator, name, icon, tech_name, unit, device_class, SensorStateClass.TOTAL_INCREASING)
@@ -128,10 +206,6 @@ class WaterMeterSensor(MeterSensor):
     def __init__(self, coordinator, name, icon, tech_name):
         super().__init__(coordinator, name, icon, tech_name, UnitOfVolume.CUBIC_METERS, SensorDeviceClass.WATER)
 
-class EnergyMeterSensor(MeterSensor):
-    def __init__(self, coordinator, name, icon, tech_name):
-        super().__init__(coordinator, name, icon, tech_name, UnitOfEnergy.KILO_WATT_HOUR, SensorDeviceClass.ENERGY)
-
 class ElectricityPriceSensor(EvoSensor):
     def __init__(self, coordinator):
         super().__init__(coordinator, "Electricity Price", "mdi:currency-eur", "", f"{CURRENCY_CENT}/{UnitOfEnergy.KILO_WATT_HOUR}", SensorDeviceClass.MONETARY)
@@ -162,9 +236,9 @@ class ElectricityPriceEuroSensor(EvoSensor):
             self._attr_extra_state_attributes["traffic_light"] = traffic_color
             super()._handle_coordinator_update()
 
-class ElectricityMeterSensor(EnergyMeterSensor):
+class ElectricityMeterSensor(MeterSensor):
     def __init__(self, coordinator):
-        super().__init__(coordinator, "Electricity consumption", "mdi:meter-electric-outline", "Verbrauch Strom")
+        super().__init__(coordinator, "Electricity consumption", "mdi:meter-electric-outline", "Verbrauch Strom", UnitOfEnergy.KILO_WATT_HOUR, SensorDeviceClass.ENERGY)
 
 class ColdWaterBathMeterSensor(WaterMeterSensor):
     """Cold Water Bath Meter Sensor specific implementation."""
@@ -195,5 +269,13 @@ class WarmWaterKitchenMeterSensor(WaterMeterSensor):
 
     def __init__(self, coordinator):
         super().__init__(coordinator, "Warm water kitchen consumption", "mdi:countertop", "Verbrauch Warmwasser Küche")
+
+
+
+
+
+
+
+
 
 
